2e9170d88292f0c6592e62fceefc5bb9ee389530
JTorrus <jtorrusgalindo@gmail.com>
2018-04-17 15:29:38 +0200

    model Key implemented for each key in the piano, audiotoolbox added as library

---
+++ iNotes/Key.swift
@@ -0,0 +1,15 @@
+//
+//  Key.swift
+//  iNotes
+//
+//  Created by Alumne on 17/4/18.
+//  Copyright © 2018 Alumne. All rights reserved.
+//
+
+import Foundation
+import AudioToolbox
+
+struct Key {
+    var note: String
+    var notePlayed: SystemSoundID
+}
